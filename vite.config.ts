55a0836d69d25da6130551d6dcb0fdbeae2fcffc
Marek <s.marekszwed@gmail.com>
2025-03-31 20:39:07 +0200

    fix(feature/MS-3): Add paths to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,14 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@components": "/src/components",
+			"@common": "/src/components/common",
+			"@constants": "/src/constants",
+			"@context": "/src/context",
+			"@helpers": "/src/helpers",
 			"@locales": "/src/locales",
 			"@pages": "/src/pages",
 			"@routes": "/src/routes",
+			"@Store": "/src/Store",
 			"@styles": "/src/styles",
 		},
 	},
