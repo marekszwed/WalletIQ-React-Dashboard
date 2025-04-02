364a74263e64edaff89c03c47dbd423414da9bc2
Marek <s.marekszwed@gmail.com>
2025-04-02 20:27:33 +0200

    fix(MS-3): rename _temp folder for proper name forms

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { default as Header } from "./Header";
 export { default as HeaderButton } from "./common/HeaderButton";
 export { default as Logo } from "./Logo";
-export { default as CardHeading } from "./_temp/CardHeading";
-export { default as LoginForm } from "./_temp/LoginForm";
-export { default as RegisterForm } from "./_temp/RegisterForm";
+export { default as CardHeading } from "./forms/CardHeading";
+export { default as LoginForm } from "./forms/LoginForm";
+export { default as RegisterForm } from "./forms/RegisterForm";
 export { default as CustomError } from "./common/CustomError";
 export { default as SideBar } from "./SideBar";
